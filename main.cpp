6d06c74e4f11db1ccf2b89f30b050cdbd584e4a7
ecuadros <ecuadros@spc.org.pe>
2026-03-31 07:39:00 -0500

    chore: update compilation command to use C++2b standard and correct path to vector.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "containers/vector.h"
 #include "containers/vector.h"
 
-// g++ main.cpp vector.cpp -o main
+// g++ -std=c++2b main.cpp containers/vector.cpp -o main
 int main(){
     DemoVector();
     return 0;

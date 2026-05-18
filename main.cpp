a4b94c3e8e4be5b03c79bb38786a99cd10699470
JunalChowdhuryG <chowdhurygomezjunaljohir@gmail.com>
2026-05-17 20:29:18 -0500

    pc3: T2 BinaryTree

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "containers/vector.h"
 #include "containers/linkedlist.h"
+#include "containers/BinaryTree.h"
 // g++ -std=c++2b main.cpp containers/vector.cpp -o main
-void ListsDemo();
+//void ListsDemo();
+void BinaryTreeDemo();
 int main(){
     // DemoVector();
     //DemoConcurrentVector();
-    ListsDemo();
+    //ListsDemo();
+    BinaryTreeDemo();
     return 0;
 }
